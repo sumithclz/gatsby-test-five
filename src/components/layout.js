6afecde336a098a4067c4d361cbf6fb26ef3997a
Sumith <sumithclz89@gmail.com>
2021-07-12 09:33:59 +0530

    ui changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,45 @@
-
-
 import * as React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import "./layout.css"
 import { Link } from 'gatsby'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
     <main>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-        </ul>
-      </nav>
-      <h1>{pageTitle}</h1>
-      {children}
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <div className="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light navbar-right">
+          <a class="navbar-brand" href="#">
+            <img src="https://www.clovatel.com/wp-content/uploads/2020/06/clovatel-logo-png-1.png" />
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0 navlinks">
+            <li className="navlinkitem">
+              <Link to="/" className="navlinktext"> Home </Link>
+            </li>
+            <li className="navlinkitem">
+              <Link to="/about" className="navlinktext"> About </Link>
+            </li>
+            <li className="navlinkitem">
+              <Link to="/blog" className="navlinktext"> Blog </Link>
+            </li>
+          </ul>
+          </div>
+        </nav>
+      </div>
+      <div className="main-wrapper"> {children} </div>
     </main>
   )
 }
