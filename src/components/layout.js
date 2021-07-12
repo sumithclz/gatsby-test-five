6ba7f770b8cac90afd1716fc6936cedc989cebe1
Sumith <sumithclz89@gmail.com>
2021-07-12 11:11:43 +0530

    llp

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,10 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import "./layout.css"
 import { Link } from 'gatsby'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
